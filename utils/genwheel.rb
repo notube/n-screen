51b03178990ec0c060e4957f736a7fbc36d91f12
Dan Brickley <danbri@danbri.org>
2011-06-24 16:41:49 +0100

    carousel

---
+++ utils/genwheel.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+# TellyClub:n-screen danbri$ find images/ -name \*.jpg  -exec convert -define jpeg:size=500x180  {}  -auto-orient  -thumbnail 128x   -unsharp 0x.5  thumbs/{} \;              
+
+# Quick script to generate bulk of a page suitable for use with http://www.professorcloud.com/mainsite/carousel-integration.htm
+#
+# 
+# Dan Brickley <danbri@danbri.org>
+
+img = `ls thumbs/images/*jpg`
+
+tpl = '<img class="cloudcarousel" src="URL" alt="DESC" title="TITLE" />'
+
+img.each do |i|
+  i.chomp!
+#  entry = "#{tpl}"
+  entry = tpl.clone
+  entry.gsub!(/URL/, "#{i}" )
+  l = i.clone
+  l.gsub!(/_/," ")
+  l.gsub!(/thumbs\/images\//,'')
+  l.gsub!(/.jpg/,'')
+  entry.gsub!(/DESC/, l )
+  entry.gsub!(/TITLE/, l )
+  puts "#{entry}\n"
+end
